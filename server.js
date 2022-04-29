230e22a9b92a079b79f78e284a47249dad6bc1bb
atmason90 <atmason90@gmail.com>
2022-04-28 22:28:08 -0700

    install console.table

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 // Dependencies
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+const cTable = require('console.table');
 
+// Establish connection to mysql
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -10,3 +12,5 @@ const connection = mysql.createConnection({
     },
     console.log('Connected to the ems_db database')
 );
+
+
